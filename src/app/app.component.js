e6ae830bb64c1ef6bdf4034182b63ed1c92929a6
Reza Alemy <ralemy@impinj.com>
2016-08-26 17:48:25 -0700

    Added Hero class and its tests

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,11 +1,22 @@
 import {Component} from "@angular/core";
 import template from "./app.component.pug";
 
+
+export class Hero {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
 @Component({
     selector: "my-app",
     template: template()
 })
 export class AppComponent {
-    title = "Tour of Heros";
-    hero = "Windstrom";
+    constructor() {
+        this.title = "Tour of Heros";
+        this.hero = new Hero(1, "Windstrom");
+    }
 }
+
--- src/app/app.component.spec.js
+++ src/app/app.component.spec.js
@@ -11,12 +11,12 @@ import {
 
 import {provide} from "@angular/core";
 
-import {AppComponent} from "./app.component";
+import {AppComponent, Hero} from "./app.component";
 import {AppModule} from "./app.module";
 
 let fixture = null;
 describe("App Component", function () {
-//    "use strict";
+    "use strict";
 
     beforeEach(()=> {
         TestBed.configureTestingModule({
@@ -34,7 +34,7 @@ describe("App Component", function () {
 
     it("Should have a hero", async(()=> {
         this.fixture.whenStable().then(()=> {
-            expect(this.fixture.componentInstance.hero).toEqual("Windstrom");
+            expect(this.fixture.componentInstance.hero).toBeDefined();
         });
     }));
 
@@ -44,16 +44,29 @@ describe("App Component", function () {
                 component = this.fixture.componentInstance;
             this.fixture.detectChanges();
 
-            expect(element.children.length).toEqual(2,
-                "Should have two children");
+            expect(element.children.length).toEqual(4,
+                "Should have at least two children");
             expect(element.children[0].tagName).toEqual("H1",
                 "First child should be H1 tag");
             expect(element.children[1].tagName).toEqual("H2",
                 "Second child should be H2 tag");
             expect(element.children[0].textContent).toEqual(component.title,
                 "First element should contain the title");
-            expect(element.children[1].textContent).toMatch(new RegExp(`^\\s*${component.hero}`),
-                "Second element should start with hero's name");
+            expect(element.children[1].textContent).toMatch(new RegExp(`${component.hero.name}\\s*details!`),
+                "Second element is the header for hero details");
+            expect(element.children[2].textContent).toMatch(new RegExp(`id:\\s*${component.hero.id}`),
+                "Third element should contain hero id");
+            expect(element.children[3].textContent).toMatch(new RegExp(`name:\\s*${component.hero.name}`),
+                "Fourth element should contain hero name");
         });
     }));
+});
+
+describe("Hero Class", function () {
+    "use strict";
+    it("Should have an id and a name", ()=> {
+        const hero = new Hero(1,"someHero");
+        expect(hero.id).toEqual(1, "hero id should be 1");
+        expect(hero.name).toEqual("someHero","hero name should be someHero");
+    });
 });
